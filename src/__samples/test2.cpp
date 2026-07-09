ebe9386ab18ae33c3d2bceeb30355a26c6afb339
Sonnnn <12345678+username@users.noreply.github.com>
2026-07-09 17:47:46 +0100

    create file for my test work, and drafted up todos

---
+++ src/__samples/test2.cpp
@@ -0,0 +1,173 @@
+/*  date = July 09th 2026  */
+
+/*
+NOTE(S): Not using any of your abstractions yet, going super simple only using the base layer. When I have something I'm happy with, I'll integrate with your os layer.
+TODO(S): Need to use same tab size, also I think my editor doesn't sub out spaces for tabs.
+*/
+
+void OutputDebugStringF(const char* fmt, ...);
+
+#include "core/core_include.h"
+#include "core/core_include.cpp"
+
+
+/*
+Brain dump.
+
+---Session Semantics----
+Session: 
+- continuous period where an application's process exists
+Session starts when: 
+- a process first appears.
+Sessions ends when:
+- process exits
+- pc shuts down/crashes (recovered on next launch via temp session at hte last heartbeat)
+- (will htink about later) app has been inactive for a long period to support suspension
+
+--- Store per Session ----
+- app name
+- exe name
+- exe path
+- icon
+- pid
+- ppid
+- start timestamp
+- end timestamp
+- duration
+- peak cpu usage		( we can think later how to do the graph )
+- peak gpu usage
+- peak memory usage
+- focused duration (optional)
+- background duration (optional)
+- recovered from crash flag
+
+---- Persistence ----
+When a session starts:
+- create a temp session file
+Every 5-10 seconds (can expose this to user)
+- update elapsed duration
+- update peak CPU/GPU/MEM
+- flush to disk
+When session ends:
+- finalise session and make it permament
+- delete temp file
+On application startup:
+- look for unfinished temp sessions
+- recover them
+- end them at the last heartbeat timestamp
+- mark as recovered_from_crash
+
+---- UI ----
+Default view should be daily totals.
+e.g.
+16 Apr 2026
+Fortnite 4h 12min
+VS Code  2h 18min
+Spotify  1h 05min
+
+These can be expanded if needed to show indivual sessions in the day
+e.g.
+Fortnite
+18:02 → 19:10   1h 08m
+19:22 → 21:05   1h 43m
+21:20 → 22:14   54m
+
+The user should be able to aggregrate based on daily/weekly/monthly.
+
+---- Short Sessions ----
+Should track every session internally and only display sessions if their duration >= 30 seconds.
+However, very short launches can be either hidden or appear as "N quick launches"
+
+---- Session merging rules ----
+Merge consecutive sessions when:
+- same exe path AND
+- new process starts withing 10-30 seconds AND
+- previous process wasn't intentionally closed (updates might change exe path, so treat as new app?)
+Why?
+- to avoid launcher/game restarts becoming multiple sessions
+- to avoid update or crashes splitting one play session
+
+Merge simulatenous sessions when:
+- treat as one application session
+- track all pids of the same exe
+- end session when last pid exits
+
+---- Edge Cases ----
+Crash or power loss
+- recover unifinished session from temp file
+- end session at last saved heartbeat
+App restarts itself
+- merge into previous session if merge rules match
+e.g.
+Game.exe
+|
+V
+Game exits
+|	(4 seconds)
+V
+Game.exe starts again
+
+= one session
+
+Launcher starts game. 
+- E.g. steam starts game.exe. Need to track seperately. Might be a non-issue and should just work.
+
+Midnight
+- don't split sessions
+- store 23:30 -> 01:15
+- when displaying daily sesisons, do the calculation yourself
+e.g. 
+15 Apr = 30 min
+16 Apr = 1h 15min
+
+System Sleep
+- record sleep start
+- record wake time
+Either
+- exclude sleep from duration ( simple, i like this)
+- make it user configurable (can think about later)
+
+Clock changes
+- store timestamps in UTC
+- convert to local time only when displaying
+
+Executable moved
+- identify apps by exe path initially
+- later I can introduce a stable app id so moving/installing doesnt create a "new" app (whenever an app is opened for the first time i get its name and give it an id? will think about this later)
+
+Renamed exe
+- if exe name/path changed after an update
+-- consider as new app
+-- look into "smarter" matching later
+
+
+*/
+int main()
+{
+
+
+
+
+return 0;
+}
+
+///////////////////////////////////////////////////////////
+// - Main helpers
+//
+// todo: Code for this is bad
+void OutputDebugStringF(const char* fmt, ...)
+{
+  #if DEBUG_MODE
+  va_list argptr;
+  va_start(argptr, fmt);
+  Scratch scratch = get_scratch(0, 0);
+  Data_buffer buffer = data_buffer_make(scratch.arena, 128);
+  int ret = vsprintf_s((char*)buffer.data, buffer.count, fmt, argptr);
+  if (ret >= 0 && ret < buffer.count)
+  {
+    OutputDebugStringA((char*)buffer.data);
+  } else { InvalidCodePath(); }
+  end_scratch(&scratch);
+  va_end(argptr);
+  #endif
+}
